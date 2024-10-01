14bd9b6004ee681ec63ad0bbb6cd5cea01f5693e
YakuninDmitriy <dmitriy.yakunin1@gmail.com>
2024-10-01 12:54:10 +0300

    01.10.2024 рабочая версия

--- scripts.js
+++ scripts.js
@@ -3,23 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const projectsBtn = document.querySelector('.projects__btn');
     const asideMenu = document.querySelector('#aside__menu');
 
-    // Скрытие и появление подменю
-    projectsBtn.addEventListener('click', function() {
-      if (asideMenu.classList.contains('visibility__hidden')) {
-        asideMenu.innerHTML = "<div class='projects'><h2>Projects</h2><div class='project__list'><div class='project project__1'><img src='#' alt='i' class='project__1__logo'>Default project<button class='project__menu__btn'>1</button></div></div></div>"
-        asideMenu.classList.remove('visibility__hidden')
-        asideMenu.classList.add('visibility__visible')
-        
+    // Функция для скрытия и появления элемента 
+    //(ДОБАВИТЬ ОТРИСОВКУ HTML С ПОМОЩЬЮ 2 АРГУМЕНТА)
+
+    const hideVisibleElements = (elem) => {
+      if (elem.classList.contains('visibility__hidden')) {
+        // asideMenu.innerHTML = "<div class='projects'><h2>Projects</h2><div class='project__list'><div class='project project__1'><img src='#' alt='i' class='project__1__logo'>Default project<button class='project__menu__btn'>1</button></div></div></div>"
+        elem.classList.remove('visibility__hidden')
+        elem.classList.add('visibility__visible')
       }
       else {
-        asideMenu.classList.remove('visibility__visible')
-        asideMenu.classList.add('visibility__hidden')
+        elem.classList.remove('visibility__visible')
+        elem.classList.add('visibility__hidden')
       }
-    });
+    };
     
-    
-    
-    
+    projectsBtn.addEventListener('click', () => hideVisibleElements(asideMenu));
+      
     
 
 });
+
