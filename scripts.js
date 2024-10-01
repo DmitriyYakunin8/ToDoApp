4aa1f4b1862ba0d4f37c1252a401b36c9e774213
YakuninDmitriy <dmitriy.yakunin1@gmail.com>
2024-10-01 13:26:08 +0300

    01.10.2024 final

--- scripts.js
+++ scripts.js
@@ -1,26 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     const projectsBtn = document.querySelector('.projects__btn');
-    const asideMenu = document.querySelector('#aside__menu');
+    const asideMenu = document.querySelector('.aside__menu');
 
     // Функция для скрытия и появления элемента 
     //(ДОБАВИТЬ ОТРИСОВКУ HTML С ПОМОЩЬЮ 2 АРГУМЕНТА)
 
     const hideVisibleElements = (elem) => {
-      if (elem.classList.contains('visibility__hidden')) {
-        // asideMenu.innerHTML = "<div class='projects'><h2>Projects</h2><div class='project__list'><div class='project project__1'><img src='#' alt='i' class='project__1__logo'>Default project<button class='project__menu__btn'>1</button></div></div></div>"
-        elem.classList.remove('visibility__hidden')
-      }
-      else {
-        elem.classList.add('visibility__hidden')
-      }
+      elem.classList.contains('visibility__hidden') ? elem.classList.remove('visibility__hidden') : elem.classList.add('visibility__hidden');
     };
-    
+
     projectsBtn.addEventListener('click', () => hideVisibleElements(asideMenu));
       
+    // Добавление нового проекта
+    let addNewProject = document.getElementById('add__new__project')
 
-
-
+    addNewProject.addEventListener('click', () => {
+      let project__list = document.querySelector('.project__list')
+      project__list.innerHTML += ' <div class="project project__new"><img src="#" alt="i" class="project__1__logo">New project<button class="project__menu__btn">1</button></div>'
+    })
+    
 
 
 });
