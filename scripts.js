fa01dc2da2e96039a6f2a89669cac86d38d1ec49
YakuninDmitriy <dmitriy.yakunin1@gmail.com>
2024-10-08 11:45:42 +0300

    08.10.2024

--- scripts.js
+++ scripts.js
@@ -12,19 +12,58 @@ document.addEventListener('DOMContentLoaded', function() {
 
     projectsBtn.addEventListener('click', () => hideVisibleElements(asideMenu));
       
-    // Добавление нового проекта
+    // Добавление проекта
+    let project__list = document.querySelector('.project__list')
     let addNewProject = document.getElementById('add__new__project')
 
+    let projectsArray = [1];
+
     addNewProject.addEventListener('click', () => {
-      let project__list = document.querySelector('.project__list')
-      project__list.innerHTML += ' <div class="project project__new"><img src="#" alt="i" class="project__1__logo">New project<button class="project__menu__btn">1</button></div>'
+      let last = projectsArray.at(-1);
+      projectsArray.push(last + 1)
+      projectsArray.shift()
+      console.log(projectsArray[0]);
+                
+      project__list.innerHTML += `<div class="project__${projectsArray[0]}">
+                        <div class="project">
+                            <div class="project__logo">
+                                <img src="/icons/defaultProjectLogo.png" alt="#" id="project__logo">
+                            </div>
+                            <div class="project__inList__name">
+                                Default project name
+                            </div>
+                            <div class="project__options__btn">
+                                <img id="project__options__btn__img" src="/icons/projectOptionsBtn.png" alt="icon">
+                            </div>
+                        </div>
+                        <div class="project__options">
+                            <div class="project__option remove__btn">Remove</div>
+                            <div class="project__option">Rename</div>
+                            <div class="project__option">Change logo</div>
+                        </div>
+                    </div>`
+
+    // Обязательные функции для возможности управления вновь добавленными проектами
+    removeProject()  
     })
     
-   const projectOptionsBtn = document.querySelectorAll('.project__options__btn')
+    //Удаление проекта
 
-   function findCorrectProject(project) {   
-      let child = 
-   }
+    function removeProject() {
+      let removeBtns = document.querySelectorAll('.remove__btn')
+    
+      for (let i = 0; i < removeBtns.length; i++) {
+        removeBtns[i].addEventListener('click', () => {
+          removeBtns[i].parentNode.parentNode.remove()
+        
+        projectsArray[0] = projectsArray[0] - 1
+        })
+      }
+    }
+  
+    removeProject()  
+
+
 
 });
 
