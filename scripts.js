d1de0741f86d4955bddb6f3c935cce001ff05d2d
YakuninDmitriy <dmitriy.yakunin1@gmail.com>
2024-10-09 12:37:48 +0300

    09.10.2024

--- scripts.js
+++ scripts.js
@@ -11,6 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     projectsBtn.addEventListener('click', () => hideVisibleElements(asideMenu));
+
+    // НАСТРОЙКА ASIDE MENU
       
     // Добавление проекта
     let project__list = document.querySelector('.project__list')
@@ -24,8 +26,8 @@ document.addEventListener('DOMContentLoaded', function() {
       projectsArray.shift()
       console.log(projectsArray[0]);
                 
-      project__list.innerHTML += `<div class="project__${projectsArray[0]}">
-                        <div class="project">
+      project__list.innerHTML += `
+                        <div class="project" id="project__${projectsArray[0]}">
                             <div class="project__logo">
                                 <img src="/icons/defaultProjectLogo.png" alt="#" id="project__logo">
                             </div>
@@ -36,15 +38,15 @@ document.addEventListener('DOMContentLoaded', function() {
                                 <img id="project__options__btn__img" src="/icons/projectOptionsBtn.png" alt="icon">
                             </div>
                         </div>
-                        <div class="project__options">
+                        <div class="visibility__hidden project__options">
                             <div class="project__option remove__btn">Remove</div>
                             <div class="project__option">Rename</div>
                             <div class="project__option">Change logo</div>
-                        </div>
-                    </div>`
+                        </div>`
 
     // Обязательные функции для возможности управления вновь добавленными проектами
     removeProject()  
+    projectsOptions()
     })
     
     //Удаление проекта
@@ -55,15 +57,49 @@ document.addEventListener('DOMContentLoaded', function() {
       for (let i = 0; i < removeBtns.length; i++) {
         removeBtns[i].addEventListener('click', () => {
           removeBtns[i].parentNode.parentNode.remove()
-        
-        projectsArray[0] = projectsArray[0] - 1
+          projectsArray[0] = projectsArray[0] - 1
         })
       }
     }
   
-    removeProject()  
+    removeProject();  
 
+    // Опции проекта
+    
+    function projectsOptions() {
+      let optionsBtns = document.querySelectorAll('.project__options__btn')
 
+      for(let i = 0; i < optionsBtns.length; i++) {
+        optionsBtns[i].addEventListener('click', () => hideVisibleElements(optionsBtns[i].parentElement.nextElementSibling))
+      }
+    };
 
+    projectsOptions();
+
+    const projects = document.querySelectorAll('.project');
+
+    projects.forEach((project) => {
+        project.addEventListener('mouseenter', function() {
+            const projectId = this.id;
+            console.log(this.id);
+            
+            const projectOptions = document.querySelector(`#${projectId} .project__options`);
+            
+            if (!projectOptions.classList.contains('visibility__hidden')) return;
+            
+            projectOptions.classList.remove('visibility__hidden');
+            projectOptions.style.top = `${this.offsetHeight}px`;
+        });
+
+        project.addEventListener('mouseleave', function() {
+            const projectId = this.id;
+            const projectOptions = document.querySelector(`#${projectId} .project__options`);
+            
+            if (projectOptions.classList.contains('visibility__hidden')) return;
+            
+            projectOptions.classList.add('visibility__hidden');
+            projectOptions.style.top = '100%';
+        });
+    });
 });
 
