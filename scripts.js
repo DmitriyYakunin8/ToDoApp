a9a01e9d08ff584911406f52e4972d9df0ec9e8f
YakuninDmitriy <dmitriy.yakunin1@gmail.com>
2024-10-11 13:34:19 +0300

    11.10.2024

--- scripts.js
+++ scripts.js
@@ -15,55 +15,47 @@ document.addEventListener('DOMContentLoaded', function() {
     // НАСТРОЙКА ASIDE MENU
       
     // Добавление проекта
-    let project__list = document.querySelector('.project__list')
-    let addNewProject = document.getElementById('add__new__project')
 
-    let projectsArray = [1];
+    let projectIdsArray = [1];
 
-    addNewProject.addEventListener('click', () => {
-      let last = projectsArray.at(-1);
-      projectsArray.push(last + 1)
-      projectsArray.shift()
-      console.log(projectsArray[0]);
-                
-      project__list.innerHTML += `
-                        <div class="project" id="project__${projectsArray[0]}">
-                            <div class="project__logo">
-                                <img src="/icons/defaultProjectLogo.png" alt="#" id="project__logo">
-                            </div>
-                            <div class="project__inList__name">
-                                Default project name
-                            </div>
-                            <div class="project__options__btn">
-                                <img id="project__options__btn__img" src="/icons/projectOptionsBtn.png" alt="icon">
-                            </div>
-                        </div>
-                        <div class="visibility__hidden project__options">
-                            <div class="project__option remove__btn">Remove</div>
-                            <div class="project__option">Rename</div>
-                            <div class="project__option">Change logo</div>
-                        </div>`
+    function addNewProjectFunc() {
+      let project__list = document.querySelector('.project__list')
+      let addNewProject = document.getElementById('add__new__project')
 
-    // Обязательные функции для возможности управления вновь добавленными проектами
-    removeProject()  
-    projectsOptions()
-    })
-    
-    //Удаление проекта
+      addNewProject.addEventListener('click', () => {
+        let last = projectIdsArray.at(-1);
+        projectIdsArray.push(last + 1)
+        projectIdsArray.shift()
+        console.log(projectIdsArray[0]);
+                  
+        project__list.innerHTML += `
+                          <div class="project" id="project__${projectIdsArray[0]}">
+                              <div class="project__logo">
+                                  <img src="/icons/defaultProjectLogo.png" alt="#" id="project__logo">
+                              </div>
+                              <div class="project__inList__name">
+                                  Default project name
+                              </div>
+                              <div class="project__options__btn">
+                                  <img id="project__options__btn__img" src="/icons/projectOptionsBtn.png" alt="icon">
+                              </div>
+                          </div>
+                          <div class="visibility__hidden project__options">
+                              <div class="project__option remove__btn">Remove</div>
+                              <div class="project__option">Rename</div>
+                              <div class="project__option">Change logo</div>
+                          </div>`
 
-    function removeProject() {
-      let removeBtns = document.querySelectorAll('.remove__btn')
-    
-      for (let i = 0; i < removeBtns.length; i++) {
-        removeBtns[i].addEventListener('click', () => {
-          removeBtns[i].parentNode.parentNode.remove()
-          projectsArray[0] = projectsArray[0] - 1
-        })
-      }
+      // Обязательные функции для возможности управления вновь добавленными проектами
+
+      projectsOptions()
+      optionsPositioning()
+      removeProject()  
+      })
     }
-  
-    removeProject();  
 
+    addNewProjectFunc()
+    
     // Опции проекта
     
     function projectsOptions() {
@@ -76,30 +68,64 @@ document.addEventListener('DOMContentLoaded', function() {
 
     projectsOptions();
 
-    const projects = document.querySelectorAll('.project');
+    // Позиционирование опций относительно проекта
 
-    projects.forEach((project) => {
-        project.addEventListener('mouseenter', function() {
-            const projectId = this.id;
-            console.log(this.id);
-            
-            const projectOptions = document.querySelector(`#${projectId} .project__options`);
-            
-            if (!projectOptions.classList.contains('visibility__hidden')) return;
-            
-            projectOptions.classList.remove('visibility__hidden');
-            projectOptions.style.top = `${this.offsetHeight}px`;
-        });
+    function optionsPositioning() {
+      const projects = document.querySelectorAll('.project');
 
-        project.addEventListener('mouseleave', function() {
-            const projectId = this.id;
-            const projectOptions = document.querySelector(`#${projectId} .project__options`);
+      projects.forEach((project) => {
+          project.addEventListener('mouseenter', () => {
+           let options = project.nextElementSibling
+          
+            // options.style.left = project.getBoundingClientRect().left + options.getBoundingClientRect().width + 'px'
+            options.style.top = project.getBoundingClientRect().top + options.getBoundingClientRect().height / 3 + 'px'
+
+            const valueInPx = parseFloat(window.getComputedStyle(options).getPropertyValue('left'));
+            const fontSize = parseFloat(window.getComputedStyle(project, null).getPropertyValue("font-size"));
+            const conversionFactor = valueInPx / fontSize;
+            const valueInEm = conversionFactor * (fontSize || 16);
+        
+            options.style.left = valueInEm
+            console.log(window.getComputedStyle(options).getPropertyValue('left'));
             
-            if (projectOptions.classList.contains('visibility__hidden')) return;
-            
-            projectOptions.classList.add('visibility__hidden');
-            projectOptions.style.top = '100%';
-        });
-    });
+
+          })
+      });
+    }
+    
+    optionsPositioning()
+   
+    //Удаление проекта
+
+    function removeProject() {
+      let removeBtns = document.querySelectorAll('.remove__btn')
+    
+      for (let i = 0; i < removeBtns.length; i++) {
+        removeBtns[i].addEventListener('click', () => {
+         
+          removeBtns[i].parentNode.previousElementSibling.remove()
+          removeBtns[i].parentNode.remove()
+          projectIdsArray[0] = projectIdsArray[0] - 1
+        })
+      }
+    }
+  
+    removeProject();  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
