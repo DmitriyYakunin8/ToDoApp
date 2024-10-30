d8a2353183e952973e33cabc625af497398038ec
YakuninDmitriy <dmitriy.yakunin1@gmail.com>
2024-10-30 14:06:53 +0300

    30.10.2024

--- scripts.js
+++ scripts.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
       recursion(elem);
 
       for (let value of classes) {
-        arrOfClasses.push(value)
+        arrOfClasses.push(value);
       }
 
       return arrOfClasses;
@@ -41,25 +41,25 @@ document.addEventListener('DOMContentLoaded', function() {
     let projectIdsArray = [1];
 
     function addNewProjectFunc() {
-      let project__list = document.querySelector('.project__list')
-      let addNewProject = document.getElementById('add__new__project')
+      let project__list = document.querySelector('.project__list');
+      let addNewProject = document.getElementById('add__new__project');
 
       addNewProject.addEventListener('click', () => {
         let last = projectIdsArray.at(-1);
-        projectIdsArray.push(last + 1)
-        projectIdsArray.shift()
+        projectIdsArray.push(last + 1);
+        projectIdsArray.shift();
         console.log(projectIdsArray[0]);
                   
         project__list.innerHTML += `
                           <div class="project" id="project__${projectIdsArray[0]}">
                               <div class="project__logo">
-                                  <img src="/icons/defaultProjectLogo.png" alt="#" id="project__logo">
+                                  <img src="/icons/defaultProjectLogo.png" alt="#" class="project__logo__img">
                               </div>
                               <div class="project__inList__name">
                                   Default project name
                               </div>
                               <div class="project__options__btn">
-                                  <img id="project__options__btn__img" src="/icons/projectOptionsBtn.png" alt="icon">
+                                  <img class="project__options__btn__img" src="/icons/projectOptionsBtn.png" alt="icon">
                               </div>
                           </div>
                           <div class="project__options visibility__hidden">
@@ -69,23 +69,22 @@ document.addEventListener('DOMContentLoaded', function() {
                           </div>`
 
       // Обязательные функции для возможности управления вновь добавленными проектами
-      projectsOptions()
-      optionsPositioning()
-      removeProject() 
-      renameProject()
-      changeProjectLogo()
-      // focusOnElement(document.querySelector('.projects__logo'))
+      projectsOptions();
+      optionsPositioning();
+      removeProject();
+      renameProject();
+      changeProjectLogo();
       })
     }
 
-    addNewProjectFunc()
+    addNewProjectFunc();
     
     // Опции проекта
     function projectsOptions() {
-      let optionsBtns = document.querySelectorAll('.project__options__btn')
+      let optionsBtns = document.querySelectorAll('.project__options__btn');
 
       for(let i = 0; i < optionsBtns.length; i++) {
-        optionsBtns[i].addEventListener('click', () => hideVisibleElements(optionsBtns[i].parentElement.nextElementSibling))
+        optionsBtns[i].addEventListener('click', () => hideVisibleElements(optionsBtns[i].parentElement.nextElementSibling));
       }
     };
 
@@ -97,25 +96,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
       projects.forEach((project) => {
           project.addEventListener('mouseenter', () => {
-           let options = project.nextElementSibling
+           let options = project.nextElementSibling;
           
-            options.style.top = project.getBoundingClientRect().top + options.getBoundingClientRect().height / 3 + 'px'
-            options.style.left = project.getBoundingClientRect().height * 3.9 + 'px'
+            options.style.top = project.getBoundingClientRect().top + options.getBoundingClientRect().height / 3 + 'px';
+            options.style.left = project.getBoundingClientRect().height * 3.9 + 'px';
              })
       });
     }
     
-    optionsPositioning()
+    optionsPositioning();
    
     //Удаление проекта
     function removeProject() {
-      let removeBtns = document.querySelectorAll('.remove__btn')
+      let removeBtns = document.querySelectorAll('.remove__btn');
     
       for (let i = 0; i < removeBtns.length; i++) {
         removeBtns[i].addEventListener('click', () => {
-          removeBtns[i].parentNode.previousElementSibling.remove()
-          removeBtns[i].parentNode.remove()
-          projectIdsArray[0] = projectIdsArray[0] - 1
+          removeBtns[i].parentNode.previousElementSibling.remove();
+          removeBtns[i].parentNode.remove();
+          projectIdsArray[0] = projectIdsArray[0] - 1;
         })
       }
     }
@@ -124,19 +123,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Переименование проекта
     function renameProject() {
-      let renameBtns = document.querySelectorAll('.rename__btn')
+      let renameBtns = document.querySelectorAll('.rename__btn');
 
       for(let i = 0; i < renameBtns.length; i++) {
         renameBtns[i].addEventListener('click', () => {
-          let projectInListName = renameBtns[i].parentElement.previousElementSibling.querySelector('.project__inList__name')
-          projectInListName.innerHTML = `<input type="text" id="project__${i + 1}__name">`
-          let projectsOptions = renameBtns[i].parentElement
-          hideVisibleElements(projectsOptions)
+          let projectInListName = renameBtns[i].parentElement.previousElementSibling.querySelector('.project__inList__name');
+          projectInListName.innerHTML = `<input type="text" id="project__${i + 1}__name">`;
+          let projectsOptions = renameBtns[i].parentElement;
+          hideVisibleElements(projectsOptions);
                     
-          let input = document.querySelector(`#project__${i + 1}__name`)
-          input.style.backgroundColor = window.getComputedStyle(renameBtns[i]).backgroundColor
+          let input = document.querySelector(`#project__${i + 1}__name`);
+          input.style.backgroundColor = window.getComputedStyle(renameBtns[i]).backgroundColor;
           input.style.color = window.getComputedStyle(renameBtns[i].parentElement.previousElementSibling.querySelector('.project__inList__name')).color;
-          input.focus()
+          input.focus();
                    
           input.addEventListener('blur', () => {
             projectInListName.innerHTML = input.value;
@@ -145,58 +144,69 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    renameProject()
+    renameProject();
+
+    //Фокусировка на одном элементе
+    function focusOnElement(elem) {
+      let content = document.querySelector('.content');
+      let newElem = elem.cloneNode(true);
+      elem.remove();
+      content.after(newElem);
+      content.style.filter = 'blur(5px)';
+      hideVisibleElements(elem);
+    }
+
+    // Возврат фокуса на весь докумен
+    function focusAtAll(elem) {
+      let content = document.querySelector('.content');
+      content.style.filter = 'blur(0px)';
+      
+    }
 
     // Изменение логотипа проекта
     function changeProjectLogo() {
-      let projectsLogo = document.querySelector('.projects__logo')
+      // Центровка элемента с выбором лого посередине окна
+      let projectsLogo = document.querySelector('.projects__logo');
       let projectsLogoWidth = parseFloat(window.getComputedStyle(projectsLogo).width);
       let projectsLogoHeight = parseFloat(window.getComputedStyle(projectsLogo).height);
-      projectsLogo.style.left = document.documentElement.clientWidth / 2 - projectsLogoWidth / 2 + 'px'
-      projectsLogo.style.top = document.documentElement.clientHeight / 2 - projectsLogoHeight / 2 + 'px'
+      projectsLogo.style.left = document.documentElement.clientWidth / 2 - projectsLogoWidth / 2 + 'px';
+      projectsLogo.style.top = document.documentElement.clientHeight / 2 - projectsLogoHeight / 2 + 'px';
       
-      let changeLogoBtns = document.querySelectorAll('.change__logo__btn')
-
+      //Убрать/добавить невидимость и фокус на элементе с выбором лого
+      let changeLogoBtns = document.querySelectorAll('.change__logo__btn');
       for(let i = 0; i < changeLogoBtns.length; i++) {
         changeLogoBtns[i].addEventListener('click', () => {
-          hideVisibleElements(projectsLogo)
-          hideVisibleElements(changeLogoBtns[i].parentElement)
-          focusOnElement(document.querySelector('.projects__logo'))
-          // console.log(document.querySelector('.projects__logo').classList);
+          hideVisibleElements(projectsLogo);
+          hideVisibleElements(changeLogoBtns[i].parentElement);
+          focusOnElement(document.querySelector('.projects__logo'));
+        
+          //Закрыть элемент с выбором лого после клика за его пределами или выборе лого
+          let logoImgs = document.querySelectorAll('.project__logo__preview')
+            for (let j = 0; j < logoImgs.length; j++) {
+              logoImgs[j].addEventListener('click', () => {
+              let logo = changeLogoBtns[i].parentElement.previousElementSibling.querySelector('.project__logo__img')
+              console.log(logoImgs[i].src);
+              console.log(logo);
+              logo.src = logoImgs[i].src
+              focusAtAll(document.querySelector('.projects__logo'))
+              })
+            }
         })
+      
       }
+
+      
+      
+      //Замена лого на новое
+
      }
 
-    changeProjectLogo()
+    changeProjectLogo();
 
-    // Фокус на элементе   
-    // function focusOnElement(elem) {
-    //   let classes = findAllElemClasses(elem)
-    //   let string = ".content:not("
-    //   for (let i = 0; i < classes.length; i++) {
-    //       if (i < classes.length - 1) {
-    //         string += `${classes[i]}):not(`
-    //       }
-    //       else {
-    //         string += `${classes[i]}`
-    //       }
-    //   }
-
-    // let elems = document.querySelector(`${string}`)
-    // elems.style.filter = 'blur(5px)';
-    // }
     
-    // focusOnElement(document.querySelector('.projects__logo'))
  
-    function focusOnElement(elem) {
-      let content = document.querySelector('.content')
-      let newElem = elem.cloneNode(true)
-      elem.remove()
-      content.after(newElem)
-      content.style.filter = 'blur(5px)'
-    }
+    
 
-  focusOnElement(document.querySelector('.projects__logo'))
 
  
 
